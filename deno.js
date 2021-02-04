760c2c488057a38cbb55c1aad937c2cd67235376
Max Fechner <max.fechner@gmail.com>
2021-02-04 22:46:42 +0100

    update deno importing

---
+++ deno.js
@@ -0,0 +1,2 @@
+export { default as file, File } from './js/file/deno.js'
+export { default as folder, Folder } from './js/folder/deno.js'
--- js/mod.js
+++
@@ -1,2 +0,0 @@
-export { default as file, File } from './file/deno.js'
-export { default as folder, Folder } from './folder/deno.js'
