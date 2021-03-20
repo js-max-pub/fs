09364ec9a57eb93e3785f3ca64b0f09f3381a870
Max Fechner <max.fechner@gmail.com>
2021-03-20 18:40:46 +0100

    - fixed file parent folder link

--- deno/file.js
+++ deno/file.js
@@ -3,7 +3,12 @@ import { Base } from './mod.js'
 export class File extends Base {
 	type = 'file'
 	get folder() {
-		return this._folder(this.url.split('/').filter(x => x).slice(0, -1).join('/'))
+		let newURL = this.url.slice(0, this.url.lastIndexOf('/'))
+		return this._folder(newURL)
+		// console.log(url)
+		// console.log(this.url)
+		// console.log(this.url.split('/').filter(x => x).slice(0, -1).join('/'))
+		// return this._folder(this.url.split('/').filter(x => x).slice(0, -1).join('/'))
 	}
 	/**
 	 * sets append-mode for write
